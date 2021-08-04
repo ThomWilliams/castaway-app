da39a13250774449a9f7b7998212474ba801c896
Thom Williams <thomwilliams1990@gmail.com>
2021-08-04 23:34:53 +0100

    CLIENT: SRC: App.js - updated for Apollo Client and Apollo provider

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -34,42 +34,42 @@ export const ADD_USER = gql`
  }
 `;
  
-export const SAVE_PODCAST = gql`
- mutation savePodcast($podcasts: [ID]!) {
-   savePodcast(podcasts: $podcasts) {
-     podcasts {
-       _id
-       description
-       image
-       thumbnail
-       title
-       country
-       language
-       publisher
-       website
-       itunes_id
-       total_episodes
-       latest_pub_date_ms
-       earliest_pub_date_ms
-       explicit_content
-       extra {
-        properties {
-          url1
-          url2
-          url3
-          google_url
-          spotify_url
-          youtube_url
-        }
-       genre_ids {
-        items
-        example
-       }
-       episodes
-       genre {
-         name
-       }
-     }
-   }
- }
-`;
+// export const SAVE_PODCAST = gql`
+//  mutation savePodcast($podcasts: [ID]!) {
+//    savePodcast(podcasts: $podcasts) {
+//      podcasts {
+//        _id
+//        description
+//        image
+//        thumbnail
+//        title
+//        country
+//        language
+//        publisher
+//        website
+//        itunes_id
+//        total_episodes
+//        latest_pub_date_ms
+//        earliest_pub_date_ms
+//        explicit_content
+//        extra {
+//         properties {
+//           url1
+//           url2
+//           url3
+//           google_url
+//           spotify_url
+//           youtube_url
+//         }
+//        genre_ids {
+//         items
+//         example
+//        }
+//        episodes
+//        genre {
+//          name
+//        }
+//      }
+//    }
+//  }
+// `;
