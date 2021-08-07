4c6b071c9223369f382fd4bccfeb1c7281b6649d
Temi <tayolanre2000@yahoo.com>
2021-08-07 16:01:00 +0100

    FEAT: Adding node_modules to gitignore

--- client/node_modules/@apollo/client/utilities/common/canUse.js
+++
@@ -1,4 +0,0 @@
-export var canUseWeakMap = typeof WeakMap === 'function' && !(typeof navigator === 'object' &&
-    navigator.product === 'ReactNative');
-export var canUseWeakSet = typeof WeakSet === 'function';
-//# sourceMappingURL=canUse.js.map
