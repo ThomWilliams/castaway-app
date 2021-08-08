40353e4058416b00ad6f392e832b96f28421ead7
Thom Williams <thomwilliams1990@gmail.com>
2021-08-08 12:09:54 +0100

    Login / Signup fully functioning, error message added, signup error fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
@@ -16,10 +17,30 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
+
+
+// const client = new ApolloClient({
+//   uri: '/graphql',
+//   cache: new InMemoryCache(),
+// });
 
 export default class App extends React.Component {
   render() {
