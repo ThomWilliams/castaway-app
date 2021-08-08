30994073c2443b32b66a6a53df6ad0d904e8f5f4
Thom Williams <thomwilliams1990@gmail.com>
2021-08-08 20:17:47 +0100

    Search function code updated

--- client/src/components/SearchByWord/index.js
+++ client/src/components/SearchByWord/index.js
@@ -1,59 +1,120 @@
-import React, { useState } from 'react';
-import { searchResults } from "../../utils/api";
+import React, { useState } from "react";
+import { Client } from 'podcast-api';
 
 
-const SearchBar = ({setSearchData, setWrapperHeader}) => {
+const SearchBar = () => {
+  // create state for holding returned google api data
+  const [searchedPodcasts, setSearchedPodcasts] = useState([]);
+  // create state for holding our search field data
+  const [searchInput, setSearchInput] = useState("");
 
-  const [searchKeyword, setSearchKeyword] = useState("");
-  const [searchKeywordError, setSearchKeywordError] = useState("");
+  const searchPodcasts = Client({ apiKey: 'ffd40c4878f547648e7bf10c4351a68f' });
+  searchPodcasts.search({
+    q: `${searchInput}`,
+  }).then((response) => {
+    // Get response json data here
+    console.log(response.data);
+  }).catch((error) => {
+    console.log(error)
+  });
 
-  const handleSubmit = async (event) => {
-    if (event) event.preventDefault();
-    let results = [];
-    if (searchKeyword !== "") {
-      try {
-        results = await searchResults(searchKeyword);
-      } catch (e) {
-        setSearchKeywordError(e);
-      } finally {
-        if (results.length > 0) {
-          setWrapperHeader(searchKeyword);
-          setSearchData(results);
-        } else {
-          setSearchKeywordError("No matches");
-        }
+  // create method to search for Podcasts and set state on form submit
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!searchInput) {
+      return false;
+    }
+
+    try {
+      const response = await searchPodcasts(searchInput);
+
+      if (!response.ok) {
+        throw new Error("something went wrong!");
       }
-    } else {
-      setSearchKeywordError("Enter a keyword.");
+
+      const { results } = await response.json();
+
+      const podcastData = results.map((podcastResults) => ({
+        podcastId: podcastResults.id,
+        podcastLink: podcastResults.link,
+        podcastAudio: podcastResults.audio,
+        podcastImage: podcastResults.image,
+        podcastTitle: podcastResults.podcast.title_original,
+        podcastPublisher: podcastResults.podcast.publisher_original,
+        podcastDescription: podcastResults.description,
+        podcastListenNotesUrl: podcastResults.listennotes_url,
+      }));
+
+      setSearchedPodcasts(podcastData);
+      setSearchInput("");
+    } catch (err) {
+      console.error(err);
     }
   };
 
-  const onBlurHandler = () => {
-    handleSubmit();
-  };
-
-  const BarStyling = { background: "white", border: "none", width: "75vh" };
-
   return (
-    <div className="search-container">
-      <form className="form" onSubmit={handleSubmit}>
-        <input
-          style={BarStyling}
-          key="random1"
-          // value={keyword}
-          error={searchKeywordError !== "" ? true : false}
-          helperText={searchKeywordError}
-          placeholder={"Search by word"}
-          onBlur={() => onBlurHandler()}
-          onChange={(text) => setSearchKeyword(text.target.value.trim())}
-        />
-      </form>
-    </div>
+    <>
+      <section className="text-light bg-dark">
+        <div>
+          <h1>Podcast Results</h1>
+          <form onSubmit={handleFormSubmit}>
+            <div>
+              <div>
+                <input
+                  name="searchInput"
+                  value={searchInput}
+                  onChange={(e) => setSearchInput(e.target.value)}
+                  type="text"
+                  size="lg"
+                  placeholder="Search for a podcast"
+                />
+              </div>
+              <div>
+                <button type="submit" variant="success" size="lg">
+                  Submit Search
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+
+      <div>
+        <h2>
+          {searchedPodcasts.length
+            ? `Viewing ${searchedPodcasts.length} results:`
+            : "Search for a podcast"}
+        </h2>
+
+        <div>
+          {searchedPodcasts.map((podcastResults) => {
+            return (
+              <div key={podcastResults.id}>
+                {podcastResults.image ? (
+                  <image
+                    src={podcastResults.image}
+                    alt={`The cover for ${podcastResults.podcast.title_original}`}
+                    variant="top"
+                  />
+                ) : null}
+                <div>
+                  <title>{podcastResults.podcast.title_original}</title>
+                  <p>Publisher: {podcastResults.podcast.publisher_original}</p>
+                  <p>Description: {podcastResults.description}</p>
+                  <p>Podcast Link: {podcastResults.link}</p>
+                  <p>Podcast Audio: {podcastResults.audio}</p>
+                  <p>Listen Here: {podcastResults.listennotes_url}</p>
+                
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 };
 
-
-
-export default SearchBar
-
+export default SearchBar;
 
--- client/src/components/SearchByWordResults/index.js
+++ client/src/components/SearchByWordResults/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Async from 'react-async';
-import searchBar from '../SearchByWord';
+import SearchBar from '../../components/SearchByWord';
 
-// request search results from the API
+// // request search results from the API
+// const URL = 'https://listen-api.listennotes.com/api/v2/search?q=' 
+// // + `${query}`;
+// const searchResults = () =>
+//   fetch(URL, {
+//     method: "GET",
+//     headers: {
+//                "Content-type": "application/json;charset=UTF-8",
+//                "X-ListenAPI-Key": "ffd40c4878f547648e7bf10c4351a68f"
+//              },
+//   }).then(res => (res.ok ? res : Promise.reject(res)))
+//     .then(res => res.json());
+
+
 
 function SearchByWordResults({ displayAll }) {
   return (
-      <Async promiseFn={searchBar}>
+      <Async promiseFn={SearchBar}>
        {({ data, err, isLoading }) => {
           if (isLoading) return "Loading..."
           if (err) return `Something went wrong: ${err.message}`
